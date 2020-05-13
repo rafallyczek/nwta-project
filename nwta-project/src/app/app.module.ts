e0a147f8ae2bd7e6178f1f6ed306f9e6c3b02ef3
rafallyczek <rafal.lyczek@gmail.com>
2020-05-13 13:47:10 +0200

    Added routing and material modules, created page structure and added styles

--- nwta-project/src/app/app.module.ts
+++ nwta-project/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RoutingModule, routingComponents } from './routing.module';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MaterialModule,
+    RoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
