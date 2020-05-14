cc7f8305c10cbd566ac94efa8e42bd8f71ef1928
rafallyczek <rafal.lyczek@gmail.com>
2020-05-14 18:25:38 +0200

    added CORS configuration

--- restapi/src/main/java/nwta/project/restapi/controllers/BookController.java
+++ restapi/src/main/java/nwta/project/restapi/controllers/BookController.java
@@ -18,25 +18,21 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    @CrossOrigin(origins = "http://localhost:4200")
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
     }
 
     @GetMapping("/bookById/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
     public Book getBookById(@PathVariable Long id){
         return bookService.getBookById(id);
     }
 
     @PostMapping("/addBook")
-    @CrossOrigin(origins = "http://localhost:4200")
     public void addBook(@RequestBody Book book){
         bookService.addBook(book);
     }
 
     @PostMapping("deleteBook/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
     public void deleteUser(@PathVariable Long id){
         bookService.deleteBook(id);
     }
---
+++ restapi/src/main/java/nwta/project/restapi/security/CorsConfig.java
@@ -0,0 +1,18 @@
+package nwta.project.restapi.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH")
+                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+
+}
