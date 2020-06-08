32f347fdcd822019e53f03ca0a91681a5e85aace
rafallyczek <rafal.lyczek@gmail.com>
2020-06-08 08:59:03 +0200

    updated database

--- database/nwta.sql
+++ database/nwta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 29 Kwi 2020, 11:03
+-- Czas generowania: 08 Cze 2020, 08:57
 -- Wersja serwera: 10.3.15-MariaDB
 -- Wersja PHP: 7.3.6
 
@@ -42,7 +42,28 @@ CREATE TABLE `books` (
 
 INSERT INTO `books` (`id`, `title`, `author`, `year`, `genre`) VALUES
 (1, 'Spring w Akcji. Wydanie 5', 'Craig Walls', '2020', 'IT'),
-(2, 'Strażnik Kruków', 'Christopher Skaife', '2019', 'Literatura Faktu');
+(2, 'Strażnik Kruków', 'Christopher Skaife', '2019', 'Literatura Faktu'),
+(14, 'Drużyna Pierścienia', 'J.R.R.Tolkien', '1954', 'Fantastyka');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text COLLATE utf8_polish_ci NOT NULL,
+  `password` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'test', 'test'),
+(2, 'test2', 'test');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -55,6 +76,12 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,6 +89,12 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT dla tabeli `books`
 --
 ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
