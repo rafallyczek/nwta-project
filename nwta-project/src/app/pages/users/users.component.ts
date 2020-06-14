4b897d16829447ff61a59b796ba8b573bdab809d
rafallyczek <rafal.lyczek@gmail.com>
2020-06-14 13:30:04 +0200

    Added users list

--- nwta-project/src/app/pages/users/users.component.ts
+++ nwta-project/src/app/pages/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user-service/user.service';
+import { User } from 'src/app/models/user-model/user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: Array<User>;
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.getAllUsers();
+  }
+
+  getAllUsers(){
+    this.userService.getAllUsers()
+      .subscribe(
+        data => {
+          console.log(data);
+          this.users = data;
+        },
+        error => console.log(error)
+      );
   }
 
 }
